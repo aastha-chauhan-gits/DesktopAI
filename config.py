7b65cfff9e909c939382c5bd06fa1b6c3a6052fe
Aastha Chauhan <91767620+aastha-chauhan-gits@users.noreply.github.com>
2023-06-23 19:33:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-apikey = "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"
+apikey = [Add your OpenAI API key here]
 
-newsAPIkey= "a4b6c7778b4b40c49febd163fe43a424"
+newsAPIkey= [Add your News API key here]
