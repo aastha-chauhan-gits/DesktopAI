8ce8af2a1ed0f36f5a289b9c6c60054e2204fd6c
Aastha Chauhan <aastha4139@gmail.com>
2023-06-23 18:46:35 +0530

    Initial Commit

---
+++ config.py
@@ -0,0 +1,3 @@
+apikey = "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"
+
+newsAPIkey= "a4b6c7778b4b40c49febd163fe43a424"
---
+++ openaitest.py
@@ -0,0 +1,6 @@
+query= "search on google today's weather"
+
+desired_part = query.split('google',1)
+print(range(len(desired_part)))
+
+print(desired_part[1])
